131bf2b25591c35a2f92082d89f40ecf43d2887d
vankongv <vankong.v@gmail.com>
2024-09-06 13:33:53 +1000

    chore: refactor test case to use getItem for single items

--- tests/delete_item_test.go
+++ tests/delete_item_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"testing"
 
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/oolio-group/dynago"
 )
 
@@ -33,51 +32,45 @@ func TestDeleteItem(t *testing.T) {
 
 	//query all items to check they exist
 	for _, item := range items {
-		var output []user
-		_, err := table.Query(ctx, "pk = :pk and sk = :sk", map[string]types.AttributeValue{
-			":pk": &types.AttributeValueMemberS{Value: item.Pk},
-			":sk": &types.AttributeValueMemberS{Value: item.Sk},
-		}, &output)
+		var output user
+		err, found := table.GetItem(ctx, dynago.StringValue(item.Pk), dynago.StringValue(item.Sk), &output)
+
 		if err != nil {
 			t.Fatalf("expected query to succeed for item %v; got %s", item, err)
 		}
-		if len(output) == 0 {
-			t.Fatalf("expected item %v to be found, but found none", item)
+		if found == false {
+			t.Fatalf("expected item %v to be found; got none", item)
 		}
 	}
 
 	// delete 1st item
 	err := table.DeleteItem(ctx, dynago.StringValue(items[0].Pk), dynago.StringValue(items[0].Sk))
+
 	if err != nil {
-		t.Fatalf("expected delete to succeed for item %v; got %s", items[0], err)
+		t.Fatalf("failed to delete item %v; got %s", items[0], err)
 	}
 
 	// query first item to confirm it is deleted
-	var deleteOutput []user
-	_, err = table.Query(ctx, "pk = :pk and sk = :sk", map[string]types.AttributeValue{
-		":pk": &types.AttributeValueMemberS{Value: items[0].Pk},
-		":sk": &types.AttributeValueMemberS{Value: items[0].Sk},
-	}, &deleteOutput)
+	var deletedItem user
+	err, found := table.GetItem(ctx, dynago.StringValue(items[0].Pk), dynago.StringValue(items[0].Sk), &deletedItem)
 
 	if err != nil {
-		t.Fatalf("expected query to succeed for item %v after deletion; got %s", items[0], err)
+		t.Fatalf("expected query to succeed for item %v; got %s", items[0], err)
 	}
-	if len(deleteOutput) != 0 {
-		t.Fatalf("expected item %v to be deleted; found %v", items[0], deleteOutput)
+	if found == true {
+		t.Fatalf("expected item to be deleted; got %v", items[0])
 	}
 
 	// query other items to confirm they still exist
 	for i := 1; i < len(items); i++ {
-		var output []user
-		_, err := table.Query(ctx, "pk = :pk and sk = :sk", map[string]types.AttributeValue{
-			":pk": &types.AttributeValueMemberS{Value: items[i].Pk},
-			":sk": &types.AttributeValueMemberS{Value: items[i].Sk},
-		}, &output)
+		var output user
+		err, found := table.GetItem(ctx, dynago.StringValue(items[i].Pk), dynago.StringValue(items[i].Sk), &output)
+
 		if err != nil {
 			t.Fatalf("expected query to succeed for item %v; got %s", items[i], err)
 		}
-		if len(output) == 0 {
-			t.Fatalf("expected item %v to be found, but found none", items[i])
+		if found == false {
+			t.Fatalf("expected item %v to be found; got none", items[i])
 		}
 	}
 }
