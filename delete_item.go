5b8b46c2bd0811bd077dfd541003caec961a8f21
vankongv <vankong.v@gmail.com>
2024-09-10 15:57:27 +1000

    chore: revert parameter type for deleteitem

--- delete_item.go
+++ delete_item.go
@@ -14,20 +14,20 @@ import (
 * @param sk the sort key of the record
  * @return true if the record was deleted, false otherwise
 */
-func (t *Client) DeleteItem(ctx context.Context, pk Attribute, sk Attribute) error {
+func (t *Client) DeleteItem(ctx context.Context, pk string, sk string) error {
 
 	//delete item from dynamodb
 	input := &dynamodb.DeleteItemInput{
 		TableName: &t.TableName,
 		Key: map[string]types.AttributeValue{
-			"pk": pk,
-			"sk": sk,
+			"pk": &types.AttributeValueMemberS{Value: pk},
+			"sk": &types.AttributeValueMemberS{Value: sk},
 		},
 	}
-	_, err := t.client.DeleteItem(ctx, input)
+	resp, err := t.client.DeleteItem(ctx, input)
 
-	if err != nil {
-		log.Println("failed to delete record from database. Error:" + err.Error())
+	if err != nil && resp == nil {
+		log.Println("failed to delete record into database. Error:" + err.Error())
 		return err
 	}
 
