2f93ca087a6516d4cbcb384e6bc730bb486c0c84
Santhosh <nannisantosh@gmail.com>
2025-12-04 10:51:54 +0530

    chore: remove unused attributevalue import from batch_write_items.go

--- batch_write_items.go
+++ batch_write_items.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
